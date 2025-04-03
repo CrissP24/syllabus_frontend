449b9160f55f3f43edf5ededd00fbba89e985a48
CRISTHOPER DAMIAN PINCAY ALAY <128093343+CrissP24@users.noreply.github.com>
2025-04-03 10:53:25 -0500

    Update auth.service.js

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://syllabus-backend.onrender.com/api/auth/";
+const API_URL = process.env.REACT_APP_API_URL + "/api/auth/";
 
 
 class AuthService {
