4a6ac32a051dc3f850e6ad431e3c70ab0a887c37
CRISTHOPER DAMIAN PINCAY ALAY <128093343+CrissP24@users.noreply.github.com>
2025-04-07 11:32:10 -0500

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import { FaTasks, FaUsersCog, FaCalendarAlt, FaBook, FaGraduationCap } from "react-icons/fa";
-import Malla from "./Malla.component";
-import GestionSyllabus from "./gestionsyllabus.component";
+import {
+  FaTasks,
+  FaUsersCog,
+  FaCalendarAlt,
+  FaBook,
+  FaGraduationCap,
+  FaChalkboardTeacher, // Ícono para docentes principales
+} from "react-icons/fa";
 
-const Sidebar = ({ setActiveSection, activeSection }) => (
+const Sidebar = ({ setActiveSection }) => (
   <div className="sidebar-buttons">
     <Button variant="primary" className="w-100 mb-2" onClick={() => setActiveSection("actividades")}>
       <FaTasks className="me-2" />
       Gestión de Actividades
     </Button>
+
     <Button variant="success" className="w-100 mb-2" onClick={() => setActiveSection("distrifunc")}>
       <FaUsersCog className="me-2" />
       Distribución de Funciones
     </Button>
+
     <Button variant="warning" className="w-100 mb-2" onClick={() => setActiveSection("docente")}>
       <FaCalendarAlt className="me-2" />
       Gestión de Periodo
@@ -21,21 +28,24 @@ const Sidebar = ({ setActiveSection, activeSection }) => (
 
     <Button variant="secondary" className="w-100 mb-2" onClick={() => setActiveSection("datatable")}>
       <FaBook className="me-2" />
-      Programa analitico de la asignatura
+      Programa Analítico
     </Button>
-    <Button variant="secondary" className="w-100 mb-2" onClick={() => setActiveSection("GestionSyllabus")}>
+
+    <Button variant="secondary" className="w-100 mb-2" onClick={() => setActiveSection("syllabus")}>
       <FaBook className="me-2" />
-      Gestión de syllabus
+      Gestión de Syllabus
     </Button>
-    {activeSection === "gestionsyllabus" && <GestionSyllabus />}
 
     <Button variant="info" className="w-100 mb-2" onClick={() => setActiveSection("malla")}>
       <FaGraduationCap className="me-2" />
-      Malla
+      Malla Curricular
     </Button>
-    {activeSection === "malla" && <Malla />}
 
+    <Button variant="dark" className="w-100 mb-2" onClick={() => setActiveSection("docenpri")}>
+      <FaChalkboardTeacher className="me-2" />
+      Docentes Principales
+    </Button>
   </div>
 );
 
-export default Sidebar; 
+export default Sidebar;
